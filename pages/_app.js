90b639178ae81480f56535688371cdd3f7f2457d
Marcin Polak <m_polak98@o2.pl>
2023-04-13 18:03:45 +0200

    Add styles import

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
-
+import '../src/styles/main.scss'
 import Layout from "../src/components/Layout/Layout"
 
-function App({ Component, pageProps }) {
-  return <Layout><Component {...pageProps} /></Layout>
+function App({
+  Component,
+  pageProps
+}) {
+  return <Layout > < Component {
+    ...pageProps
+  }
+  /></Layout >
 }
 
 export default App
