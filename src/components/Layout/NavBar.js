e2f2933004d8460ddac30e1949312dd5bbb62ae5
fanti001 <biuro@kamedia.pl>
2023-04-16 11:02:55 +0200

    domknięcie tagu

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -1,28 +1,10 @@
 import Link from 'next/link'
 
-
 const NavBar = () => {
    return (
       <div className="nav-bar">
          <div className="nav-bar__menu-button">
-
-         
-
-         {/* <nav className='navbar-menu'>
-            <ul className='menu-list'>
-               <li>
-                  <Link href="/FeaturesPage">headphones</Link>
-               </li>
-               <li>
-                  <Link href="/PricingPage">speakers</Link>
-               </li>
-               <li>
-                  <Link href="/CommunityPage">earphones</Link>
-               </li>
-              
-
-            </ul>
-         </nav> */}
+         </div>
          <div className='nav-bar__logo'>
             <h1>audiophile</h1>
          </div>
@@ -33,4 +15,4 @@ const NavBar = () => {
    );
 }
 
-export default NavBar;
+export default NavBar
