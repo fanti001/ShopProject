5a897bd43cefe53d7eddbc235e7571d1ddfb8c8f
Wonski312 <smarciszedu@gmail.com>
2023-04-21 05:36:52 +0100

    further changes to dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import Products from '../src/components/HomePage/Products';
 import ProductDetails from '../src/components/HomePage/ProductDetails';
 import Description from '../src/components/HomePage/Description';
 
-
+import {headphones} from '../database';
 
 function HomePage() {
 	return (
 		<div
 		 className='app'
 		 >
-			<NewProduct id='01'/>
+			<NewProduct id={headphones[0].id}/>
 			<Products />
 			<ProductDetails />
 			<Description />
