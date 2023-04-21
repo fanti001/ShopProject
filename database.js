145641c210300c2b279f3ca6f50051a32ced8796
Wonski312 <smarciszedu@gmail.com>
2023-04-21 05:15:02 +0100

    menaged to make dynamic page for product

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const headphones = [
+export const headphones = [
 	{ title: "XX99 Mark II Headphones", category: "headphones", price: 2.999, id: '01' },
 	{ title: "CC Mark Headphones", category: "headphones", price: 112, id:'02' },
 	{
@@ -12,3 +12,11 @@ const headphones = [
 		price: 1231212312,id:'04'
 	},
 ];
+
+// async function handler (req,res ){
+// 	if (req.method === 'GET') {
+// 		res.status(200).json({message: headphones});
+// 	}
+// }
+
+// export default handler;
