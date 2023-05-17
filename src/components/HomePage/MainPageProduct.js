eb267cea28e2339be6c52c65e891e3c42e3a1535
Marcin Polak <m_polak98@o2.pl>
2023-05-17 18:34:43 +0200

    Add animation to homepage elements

--- src/components/HomePage/MainPageProduct.js
+++ src/components/HomePage/MainPageProduct.js
@@ -1,27 +1,42 @@
 import { headphones } from "../../../database";
 import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
 const MainPageProduct = (props) => {
 	const router = useRouter();
-
+	const [activeAnimation, setActiveAnimation] = useState(false)
 	const findProduct = () => {
-		
+
 		const product = headphones.filter(
 			(product) => product.title == props.title
 		);
-		
+
 		const productId = product[0].id;
 		router.push("/" + productId);
 	};
 
+	const productEl = useRef(null)
+
+	useEffect(() => {
+		if (productEl.current) {
+			const product = productEl.current;
+			props.windowY >= Math.floor(product.getBoundingClientRect().top + window.scrollY) ? setActiveAnimation(true) : null;
+		}
+
+	})
+
 	return (
-		<div className={props.graph}>
+
+		<div ref={productEl} className={`${props.graph} ${activeAnimation ? 'animation--active' : null}`}
+			style={{
+				animationDuration: `${props.animationDuration}`
+			}}>
 			<div className={props.bgc}></div>
 			<div className={props.desc ? props.desc : 'products-mobile'}>
 				<span>{props.title}</span>
 				{props.children}
 				<button onClick={findProduct}>see product</button>
 			</div>
-		</div>
+		</div >
 	);
 	{
 		/* {props.children} */
